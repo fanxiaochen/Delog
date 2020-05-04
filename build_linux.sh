c1df9e91d90fdd36bdfe125df705d523ed58d727
fanxiaochen <fan.daybreak@gmail.com>
2020-05-04 23:35:34 +0800

    update scripts

--- build_linux.sh
+++ build_linux.sh
@@ -1,10 +1,9 @@
-VCPKG_DIR=~/Workspace/vcpkg
 BUILD_DIR=./build
 
 if [ ! -d "${BUILD_DIR}" ]; then
     mkdir ${BUILD_DIR}
 fi
 
-cmake . -B${BUILD_DIR} -DCMAKE_TOOLCHAIN_FILE=${VCPKG_DIR}/scripts/buildsystems/vcpkg.cmake
+cmake . -B${BUILD_DIR} 
 cd ${BUILD_DIR}
 make
---
+++ build_linux_with_vcpkg.sh
@@ -0,0 +1,10 @@
+VCPKG_DIR=./vcpkg
+BUILD_DIR=./build
+
+if [ ! -d "${BUILD_DIR}" ]; then
+    mkdir ${BUILD_DIR}
+fi
+
+cmake . -B${BUILD_DIR} -DCMAKE_TOOLCHAIN_FILE=${VCPKG_DIR}/scripts/buildsystems/vcpkg.cmake
+cd ${BUILD_DIR}
+make
